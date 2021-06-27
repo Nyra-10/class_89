76c5f7db8dcb6520d0f47bda392b47f207c6870f
Nyra-10 <74824084+Nyra-10@users.noreply.github.com>
2021-06-27 19:06:01 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function adduser(){
+    player1=document.getElementById("player1name").value;
+    player2=document.getElementById("player2name").value;
+    localStorage.setItem("player1name",player1);
+    localStorage.setItem("player2name",player2);
+    window.location="game_page.html";
+}
